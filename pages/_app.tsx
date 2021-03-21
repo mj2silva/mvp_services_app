e50c43a559beb9c999a66a3914b69a6467be74c0
Ricardo Serin <serin.ricardo@gmail.com>
2021-03-21 16:51:28 -0500

    Configuración inicial de Next

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { AppProps } from 'next/app';
+import { ReactElement } from 'react';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }:AppProps):ReactElement {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
