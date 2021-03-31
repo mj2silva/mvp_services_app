dfafee948e8d9b7f628f8933595f5ceafcb2a84c
Ricardo Serin <serin.ricardo@gmail.com>
2021-03-31 11:28:08 -0500

    Creación de Sidebar y sus componentes
    Se creó el componente Sidebar que contiene los componentes: LinkSidebar y Menu

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,33 +1,22 @@
 import { FC, ReactNode } from 'react';
-import { Student } from '../../lib/types';
+import Sidebar from './Sidebar';
 import Header from './Header';
 
 type Props = {
   children: ReactNode,
-  mensaje?: string,
-  student?:Student
 }
 
-const defaultProps:Partial<Props> = {
-  mensaje: 'holis',
-  student: {
-    name: '-',
-    lastname: '-',
-  },
-};
-
-const Layout:FC<Props> = ({ children, mensaje, student }:Props) => (
-  <>
-    <div className="container">
-      <Header />
-      <div className="dashboard-container">
-        { student.name }
+const Layout:FC<Props> = ({ children }:Props) => (
+  <div className="container">
+    <Header />
+    <div className="dashboard-container">
+      <Sidebar />
+      <div>
         { children }
       </div>
+      <div><h1>Anuncios</h1></div>
     </div>
-  </>
+  </div>
 );
-
-Layout.defaultProps = defaultProps;
 
 export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import { FC } from 'react';
 
 const Home:FC = () => (
-  <div>
-    <h1>Hello Fucking World</h1>
-  </div>
+  <>
+    <div>
+      <h1>Hello Fucking World</h1>
+    </div>
+    <div>
+      <h1>Hello Fucking World</h1>
+    </div>
+    <div>
+      <h1>Hello Fucking World</h1>
+    </div>
+  </>
 );
 
 export default Home;
