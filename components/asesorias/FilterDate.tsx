4d58fec39aa01bfee3cd2d1a66f81181f65114df
Doble-V <diegovv26@gmail.com>
2021-05-18 12:26:32 -0500

    TASK-169 Maquetado de todos los modales de filtros Separacion de los filtros por componentes y agregacion de modales para cada filtro, maquetado de cada uno de estos

---
+++ components/asesorias/FilterDate.tsx
@@ -0,0 +1,51 @@
+import { FC, useState } from "react";
+import ReactDatePicker from "react-datepicker";
+import DatePicker from "../forms/formElements/DatePicker";
+import Modal from "../modal/Modal";
+
+const FilterDate: FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const openModal = () : void => setIsModalOpen(true);
+  const closeModal = () : void => setIsModalOpen(false);
+  const [startDate, setStartDate] = useState(new Date());
+  return (
+    <>
+      <button type="button" className="datatable__filter" onClick={openModal}>Fecha</button>
+      <Modal
+        title="Solicitudes por Fecha"
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+      >
+        <div className="filter">
+          <div className="filter__content">
+            <label className="form__field" htmlFor="service">
+            <span className={'form__label'}>Fecha</span>
+            <ReactDatePicker
+              popperPlacement="auto-start"
+              dropdownMode="select"
+              locale="es"
+              className="filter__input filter-datepicker"
+              calendarClassName="filter-datepicker__calendar"
+              wrapperClassName="filter-datepicker__wrapper"
+              popperClassName="filter-datepicker__popper"
+              dayClassName={() => 'filter-datepicker__day'}
+              monthClassName={() => 'filter-datepicker__month'}
+              weekDayClassName={() => 'filter-datepicker__weekday'}
+              timeClassName={() => 'filter-datepicker__time'}
+              name="Date"
+              //selected={startDate}
+              //onChange={date => setStartDate(date)}
+              onChange={() => {}}
+              autoComplete="off"
+            />
+              
+            </label>
+          </div>
+        </div>
+           
+      </Modal>
+    </>
+  )
+    
+}
+export default FilterDate;
--- components/common/Datatable.tsx
+++ components/common/Datatable.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
 import Table, { TableContent } from './Table';
+import FilterDate from '../asesorias/FilterDate'
+import FilterModality from '../asesorias/FilterModality';
+import FilterStatus from '../asesorias/FilterStatus';
 
 type Props = {
   tableContent: TableContent
@@ -9,9 +12,9 @@ const Datatable:FC<Props> = ({ tableContent }:Props) => (
   <div className="datatable">
     <div className="datatable__filters">
       <span className="datatable__filters-title">Filtros</span>
-      <button type="button" className="datatable__filter">Fecha</button>
-      <button type="button" className="datatable__filter">Modalidad</button>
-      <button type="button" className="datatable__filter">Estado</button>
+      <FilterDate />
+      <FilterModality />
+      <FilterStatus />
     </div>
     <Table content={tableContent} wrapperClass="datatable" modifier="primary" />
   </div>
